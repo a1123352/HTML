2c693f9bca8006c8870c33406fd2253ab875f597
a1123352 <a1123352@mail.nuk.edu.tw>
2025-04-25 14:20:25 +0800

    Add files via upload

---
+++ upconfirm.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset ="UTF-8">
+</head>
+
+<?php
+ echo "檔案名稱: ".$_FILES["file"]["name"]."<br/>";
+ echo "暫存檔名: ".$_FILES["file"]["tmp_name"]."<br/>";
+ echo "檔案尺寸: ".$_FILES["file"]["size"]."<br/>";
+ echo "檔案種類: ".$_FILES["file"]["type"]."<hr/>";
+ 
+ $time = time();
+ $FileName="pic\\".$time.".png";
+ 
+ if ( copy($_FILES["file"]["tmp_name"],$FileName)){
+   echo "檔案上傳成功<br/>";
+   unlink($_FILES["file"]["tmp_name"]);
+    }
+    else
+    echo "檔案上傳失敗<br/>";
+    ?>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,5 @@
+<form action="upconfirm.php" method="post"
+      enctype="multipart/form-data">
+    <input type="file" name="file"/><br>
+    <input type="submit">
+</form>
