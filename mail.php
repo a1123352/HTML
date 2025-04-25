e3a31e84d54db737240729133593bc8ff28b2b39
a1123352 <a1123352@mail.nuk.edu.tw>
2025-04-25 15:42:23 +0800

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,8 @@
+<form action ="sendmail.php" method ="post">
+to:<input type="text" name ="to"><br>
+subject:<input type="text" name ="subject"><br>
+content:<textarea name="content" id="" cols="30" rows="10">
+
+</textarea><br>
+<input type="submit">
+</form>
---
+++ sendmail.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+</head>
+<?php
+$to=$_POST["to"];
+$subject=$_POST["subject"];
+$content=$_POST["content"];
+echo $to;
+echo $subject;
+echo $content;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp@gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'a1123352@mail.nuk.edu.tw';                     //SMTP username
+    $mail->Password   = 'khij fbrl gvwu ngpk';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('a1123352@mail.nuk.edu.tw', 'Mailer');
+    //$mail->addAddress('chino931001@gmail.com', 'Joe User');     //Add a recipient
+    $mail->addAddress('chino931001@gmail.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
+</html>
